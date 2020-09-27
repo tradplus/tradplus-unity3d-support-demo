63436283481573110c393316623d99d9349bff18
fanny <service@tradplus.com>
2020-09-27 15:01:59 +0800

    V4.7.1 Heading bidding 、SDK数据返回、用户隐私规范

---
+++ Demo_v4.7.1/Assets/TradPlus/Internal/TradPlusAndroidInterstitial.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class TradPlusAndroidInterstitial
+{
+    private readonly AndroidJavaObject _interstitialPlugin;
+
+
+	public TradPlusAndroidInterstitial(string adUnitId)
+    {
+		_interstitialPlugin = new AndroidJavaObject("com.tradplus.ads.unity.InterstitialUnityPlugin", adUnitId);
+    }
+
+    //请求广告
+    public void RequestInterstitialAd(bool autoReload = false)
+    {
+        Debug.Log("_interstitialPlugin.Call");
+        _interstitialPlugin.Call("request",autoReload);
+    }
+
+    public void loadForcedlyInterstitial()
+    {
+        Debug.Log("_interstitialPlugin.Call");
+        _interstitialPlugin.Call("loadForcedly");
+    }
+
+    //展示广告
+    public void ShowInterstitialAd()
+    {
+        _interstitialPlugin.Call("show");
+    }
+
+    //展示广告（广告场景ID）
+    public void ShowInterstitialAd(string adSceneId)
+    {
+        _interstitialPlugin.Call("show", adSceneId);
+    }
+
+    //进入广告位所在界面时调用
+    public void ShowInterstitialConfirmUWSAd()
+    {
+        _interstitialPlugin.Call("confirmUWSAd");
+    }
+
+    //进入广告位所在界面时调用（广告场景ID）
+    public void ShowInterstitialConfirmUWSAd(string adSceneId)
+    {
+        _interstitialPlugin.Call("confirmUWSAd", adSceneId);
+    }
+
+    //check广告是否准备好
+    public bool IsInterstitialReady {
+        get { return _interstitialPlugin.Call<bool>("isReady"); }
+    }
+
+    //销毁广告
+    public void DestroyInterstitialAd()
+    {
+        _interstitialPlugin.Call("destroy");
+    }
+}
---
+++ Demo_v4.7.1/Assets/TradPlus/Internal/TradPlusAndroidRewardedVideo.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class TradPlusAndroidRewardedVideo
+{
+    private readonly AndroidJavaObject _rewardedVideoPlugin;
+
+
+	public TradPlusAndroidRewardedVideo(string adUnitId)
+    {
+		_rewardedVideoPlugin = new AndroidJavaObject("com.tradplus.ads.unity.RewardedVideoUnityPlugin", adUnitId);
+    }
+
+    public void RequestRewardedVideo(bool autoReload = false)
+    {
+        _rewardedVideoPlugin.Call("request",autoReload);
+    }
+
+    public void loadForcedlyRewardedVideo()
+    {
+        _rewardedVideoPlugin.Call("loadForcedly");
+    }
+
+    //展示广告
+    public void ShowRewardedVideo()
+    {
+		_rewardedVideoPlugin.Call("show");
+    }
+
+    //展示广告（广告场景ID）
+    public void ShowRewardedVideo(string adSceneId)
+    {
+        _rewardedVideoPlugin.Call("show");
+    }
+
+    //进入广告位所在界面时调用
+    public void ShowRewardedVideoConfirmUWSAd()
+    {
+        _rewardedVideoPlugin.Call("confirmUWSAd");
+    }
+
+    //进入广告位所在界面时调用（广告场景ID）
+    public void ShowRewardedVideoConfirmUWSAd(string adSceneId)
+    {
+        _rewardedVideoPlugin.Call("confirmUWSAd");
+    }
+
+    public bool IsRewardedVideoReady {
+		get { return _rewardedVideoPlugin.Call<bool>("isReady"); }
+    }
+
+
+	public void DestroyRewardedVideo()
+    {
+		_rewardedVideoPlugin.Call("destroy");
+    }
+		
+}
